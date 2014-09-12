d7be744cf4ee7385a492db75b220dc1a87bdca2b
Viktor Snesarevskiy <snesarevskiy@yandex.ru>
2014-09-12 23:29:57 +0400

    Added landscape layout and settings activity placeholder

--- src/ru/mr123150/znachit/MainActivity.java
+++ src/ru/mr123150/znachit/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -65,14 +67,36 @@ public class MainActivity extends Activity {
 		};
 		
 		countBtn.setOnClickListener(pressCount);
-		resetBtn.setOnClickListener(pressReset);
+		if(resetBtn != null){
+			resetBtn.setOnClickListener(pressReset);
+		}
 	}
 
-	/*@Override
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}*/
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+	    switch (item.getItemId()) {
+	    case R.id.action_reset:
+	    	count=0;
+			countTxt.setText(Integer.toString(count));
+			
+			Editor editor = settings.edit();
+			editor.putInt(APP_PREFERENCES_COUNT, count);
+			editor.apply();
+	        return true;
+	    case R.id.action_settings:
+	        //
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
 
 }
